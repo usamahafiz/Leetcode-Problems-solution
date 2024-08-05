6cbe412c32704f77796f0941a1ded193bc0d4494
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2024-08-05 23:35:39 +0500

    Time: 36 ms (72.86%), Space: 16.4 MB (97.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,16 +9,19 @@ class Solution:
         tail = dummy # tail means current pointer on the dummy node
         while list1 and list2: # run as long as both lists not empty
             if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+                tail.next = list1 #firstly place node of list1
+                list1 = list1.next #node of list1 also move one place ahead
             else:
                 tail.next = list2
                 list2 = list2.next
-            tail = tail.next #tail also move next node to merge both sorted lists
-        if list1:
-            tail.next = list1
-        elif list2:
+            tail = tail.next #tail also move next node to merge both sorted lists and at last None appear indicate both lists ended
+        if list1: #run as long as list1 is not empty
+            tail.next = list1 
+        elif list2: #if list1  is empty then remaining number of list2 attaches the node 
             tail.next = list2
         return dummy.next
+
+#time complexity O(n+m) bcz loop iterates each nodes of both lists 
+#space complexity O(1)
 
         
