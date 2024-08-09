0ea9ba57e50d0c428f4ce4076b37cd0deeaf8ab6
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2024-08-09 20:42:11 +0500

    Time: 46 ms (8.57%), Space: 16.6 MB (87.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,6 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.split() #split each word in a sentence with comma
-        s = s[::-1] #then slicing(reverse) the sentence
-        return " ".join(s) #at last join the string and have space between them
+        # s = s.split() #split each word in a sentence with comma
+        # s = s[::-1] #then slicing(reverse) the sentence
+        words = s.split()
+        words.reverse()
+        return " ".join(words) #at last join the string and have space between them
         
