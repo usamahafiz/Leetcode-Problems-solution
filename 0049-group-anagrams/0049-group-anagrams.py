d33d874c27eff05f2243ba6d7c1f9c45987e725b
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2024-08-21 23:26:49 +0500

    Time: 87 ms (69.42%), Space: 19.9 MB (65.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,5 +3,8 @@ class Solution:
         anagrams = defaultdict(list)
         for s in strs:
             sorted_str = "".join(sorted(s))  # words in the list sorted alphabeticaly
-            anagrams[sorted_str].append(s)
-        return anagrams.values()
+            anagrams[sorted_str].append(s) # append the words which behave as anagram on the basis of key
+        return anagrams.values() #at last return only group of anagrams 
+#time complexity O(k.n logn)
+# 'k' is the number of string and 'n' is the length of the string 
+#space complexity O(k . n) # 'k' is the number of string and 'n' is the length of the string 
