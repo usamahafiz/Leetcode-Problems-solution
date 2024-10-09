cf995aac821ca25dc42cb3fd7a1d626fdbafd1d4
Hafiz Usama <160656348+usamahafiz@users.noreply.github.com>
2024-10-10 01:00:02 +0500

    Time: 101 ms (56.3%), Space: 12.8 MB (88.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,10 @@
 class Solution(object):
     def missingNumber(self, nums):
+        # n = len(nums)
+        # for i in range ( n + 1): # in n + 1 also include the missing number
+        #     if i not in nums:
+        #         return i
         n = len(nums)
-        for i in range ( n + 1):
-            if i not in nums:
-                return i
+        expectedSum = n * ( n + 1 ) // 2
+        actualSum = sum(nums)
+        return expectedSum - actualSum
